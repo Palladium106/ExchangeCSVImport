ef43731df66b5b00a38c7e07cc2e94718960ea05
Sergey Parvov <74859778+Palladium106@users.noreply.github.com>
2021-04-21 21:10:24 +0300

    Add files via upload

---
+++ ExchangeCSVImport.ps1
@@ -0,0 +1,98 @@
+﻿#Sript for import mail contacts into AD using Exchange Management Console.
+#ExchangeCSVImport.conf file must be stored in the one folder with script.
+
+if((Get-Command New-MailContact -errorAction SilentlyContinue) -eq $null)
+{
+	Write-Error "This script must be executed on the Exchange Management Console!" 
+	return
+}
+
+#Exit if ExchangeCSVImport.conf file not found
+if((Test-Path ExchangeCSVImport.conf) -ne $true) 
+{
+	Write-Host "'ExchangeCSVImport.conf' must be stored in the one folder with script" -foregroundcolor red
+	return
+}
+
+Get-Content ".\ExchangeCSVImport.conf" | foreach-object -begin {$config=@{}} `
+	-process {
+	#Parse only strings that contains "="
+	if($_.Contains("=") -eq $True)
+	{
+		$k = $_.Split("=")
+		#Parse only strings not empty strings not beginned with "[" and "#"
+		if(($k[0].CompareTo("") -ne 0) `
+		-and ($k[0].StartsWith("[") -ne $True) `
+		-and ($k[0].StartsWith("#") -ne $True))
+		{
+			$config.Add($k[0].Trim(), $k[1].Trim())
+		}
+	}
+}
+
+Write-Host "Initial paremeters:"
+$config
+$choice=Read-Host "`nDo you want to continue? (Y/n)"
+
+#Proceed to import if user typed "y" or nothing.
+switch ($choice)
+{
+	"y" {break}
+	"" {break}
+	Default {return}
+}
+
+#Exit script if CSV file not found
+if((Test-Path $config.CSVFileName) -ne $true) 
+{
+	Write-Host "File '$($config.CSVFileName)' does not exists"
+	return
+}
+
+#Main input procedure
+Import-CSV -Path $config.CSVFileName -Delimiter "," | ForEach-Object `
+-process{
+	$FullName = $_.DisplayName
+	#Split Full User Name to place it in the right fields
+	$SplittedlName = $FullName.Split(" ",2)
+	
+	#Create contact and set basic attributes
+	try
+	{
+		New-MailContact -Name $FullName `
+			-ExternalEmailAddress $_.EmailAddress `
+			-Alias $_.SamAccountName `
+			-OrganizationalUnit $config.TargetOU `
+			-FirstName $SplittedlName[1] `
+			-LastName $SplittedlName[0]
+
+	}
+	catch [Microsoft.Exchange.Configuration.Tasks.ManagementObjectNotFoundException]
+	{
+		Write-Host "Specified OU: '$($config.TargetOU)' does not exists" -foregroundcolor red
+		exit
+	}
+	catch [Microsoft.Exchange.Data.Directory.ADObjectAlreadyExistsException]
+	{
+		Write-Host "Specified User: '$($FullName)' already exists" -foregroundcolor yellow
+	}
+	
+	#Check if the Custom Company Name is set
+	if($config.CompanyName -ne $null) {$Company = $config.CompanyName}
+	else {$Company = $_.Company}
+	
+	#Set other attributes
+	try
+	{	
+		Set-Contact $_.SamAccountName `
+			-Company $Company `
+			-Phone $_.telephoneNumber `
+			-MobilePhone $_.mobile `
+			-Department $_.Department `
+			-Title $_.Title
+	}
+	catch
+	{
+		$_.FullyQualifiedErrorID
+	}
+}
